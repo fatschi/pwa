b00db28418be2ab9c251413fd3b46225c2a1650e
Fabian Tschirschnitz <fabian.tschirschnitz@sap.com>
2018-11-26 12:45:31 +0100

    network-first

--- service-worker.js
+++ service-worker.js
@@ -4,16 +4,27 @@ workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug);
 const CACHE_NAME = 'fatschi-pwa';
 
 workbox.routing.registerRoute(
-    /.*\.(?:html)/,
+    /.*\.(?:html|js)/,
     workbox.strategies.cacheFirst({
         cacheName: `${CACHE_NAME}:network-first`,
     }),
 );
 
-// Force Caching of Opaque Responses
-// https://developers.google.com/web/tools/workbox/reference-docs/latest/workbox.cacheableResponse.Plugin
 workbox.routing.registerRoute(
     new RegExp('https://scontent\.cdninstagram\.com/'),
+    workbox.strategies.cacheFirst({
+        cacheName: `${CACHE_NAME}:network-first`,
+        plugins: [
+            // Force Cache
+            new workbox.cacheableResponse.Plugin({
+                statuses: [0, 200], // One or more status codes that a Response can have and be considered cacheable.
+            }),
+        ]
+    }),
+);
+
+workbox.routing.registerRoute(
+    new RegExp('https://api\.instagram\.com/'),
     workbox.strategies.cacheFirst({
         cacheName: `${CACHE_NAME}:network-first`,
         plugins: [
